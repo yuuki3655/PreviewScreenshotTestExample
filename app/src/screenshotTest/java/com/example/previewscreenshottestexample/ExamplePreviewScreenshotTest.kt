4fca459a1c4e108f1d4d2daa9cbb6554bf9fd3db
hummer <hummer@google.com>
2024-11-11 15:49:34 -0800

    Add example project that uses preview-screenshot-test plugin

---
+++ app/src/screenshotTest/java/com/example/previewscreenshottestexample/ExamplePreviewScreenshotTest.kt
@@ -0,0 +1,10 @@
+package com.example.previewscreenshottestexample
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun greeting() {
+    Greeting(name = "MyName")
+}
